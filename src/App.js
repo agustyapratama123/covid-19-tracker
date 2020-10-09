f3f4144949ebc96f5ab0120a66821f692d100059
agustyapratama123 <agustyapratama123@gmail.com>
2020-10-09 19:27:56 +0700

    test

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import "./App.css";
+import InfoBox from "./InfoBox";
+import Map from "./Map";
+import {
+  MenuItem,
+  Select,
+  FormControl,
+  Card,
+  CardContent,
+} from "@material-ui/core";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("WorldWide");
+
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    console.log(countryCode);
+    setCountry(countryCode);
+
+    //https://disease.sh/v3/covid-19/countries/{country}
+    //https://disease.sh/v3/covid-19/all
+  };
+
+  useEffect(() => {
+    const getCountriesData = async () => {
+      await fetch("https://disease.sh/v3/covid-19/countries")
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => ({
+            name: country.country,
+            value: country.countryInfo.iso2,
+          }));
+          setCountries(countries);
+        });
+    };
+    getCountriesData();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__left">
+        <div className="app__header">
+          <h2>COVID-19 TRACKER</h2>
+          <FormControl className="app__dropdown">
+            <Select
+              variant="outlined"
+              value={country}
+              onChange={onCountryChange}
+            >
+              <MenuItem value="WorldWide">WorldWide</MenuItem>
+              {countries.map((country) => (
+                <MenuItem value={country.value}>{country.name}</MenuItem>
+              ))}
+            </Select>
+          </FormControl>
+        </div>
+
+        <div className="app__stats">
+          <InfoBox title="Coronavirus cases" cases={2300} total={3500} />
+          <InfoBox title="Coronavirus recovered" cases={6400} total={5400} />
+          <InfoBox title="Deaths" cases={2560} total={9400} />
+          {/* infobox */}
+          {/* infobox */}
+          {/* infobox */}
+        </div>
+
+        {/* Map */}
+        <Map />
+      </div>
+      <Card className="app__right">
+        <CardContent>
+          {/* Table */}
+          <h2>Live cases by country</h2>
+          {/* Graph */}
+          <h2>WorldWide Cases</h2>
+        </CardContent>
+      </Card>
     </div>
   );
 }
